c79f3538964f9685b5305911c706543fb76d8769
Henrique Pereira <henrique.pereira@liferay.com>
2022-07-01 15:17:43 -0300

    desafio feito

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,47 @@
-export function SideBar() {
-  // Complete aqui
+import React, { useEffect, useState } from "react";
+import { api } from "../services/api";
+import { Button } from "./Button";
+
+import "../styles/sidebar.scss";
+
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+interface SideBarProps {
+  clickButton: (id: number) => void;
+  gerenId: number;
+}
+
+export function SideBar({ clickButton, gerenId }: SideBarProps) {
+  useEffect(() => {
+    api.get<GenreResponseProps[]>("genres").then((response) => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+  return (
+    <>
+      <nav className="sidebar">
+        <span>
+          Watch<p>Me</p>
+        </span>
+
+        <div className="buttons-container">
+          {genres.map((genre) => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => clickButton(genre.id)}
+              selected={gerenId === genre.id}
+            />
+          ))}
+        </div>
+      </nav>
+    </>
+  );
 }
